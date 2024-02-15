6cb2de9e98c9c2873151de9eca7f9daf48a273fd
wac20242 <160106569+wac20242@users.noreply.github.com>
2024-02-15 16:13:02 +0900

    Add files via upload

---
+++ mypath.bat
@@ -0,0 +1,12 @@
+@echo off
+:: Extract the path of the current batch file
+set myfiles_path=%~dp0
+:: Check if the path is already in the PATH variable
+for %%p in (%PATH%) do (
+    if "%%~p" == "%myfiles_path%" goto already_in_path
+)
+:: Add the path to the PATH variable (users only)
+:: setx PATH "%myfiles_path%;%PATH%"
+setx PATH "%myfiles_path%;%PATH%" >nul 2>&1
+:already_in_path
+
